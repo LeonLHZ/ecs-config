f867636656ea88d74c1129f254f2b38f01a9625c
lhz <lhz@zhengmeng.net>
2019-11-14 14:32:09 +0800

    init

---
+++ src/test/java/cn/lhz/config/EcsConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.lhz.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcsConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
